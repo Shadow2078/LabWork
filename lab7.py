c24168bdff2b211b45d458cbb46af06fde08ec17
Shadow2078 <karkisuman2021@gmail.com>
2021-12-01 21:29:13 +0545

    It's Lab7

---
+++ lab7.py
@@ -0,0 +1,9 @@
+#The sum of first 10 odd integers by the help of range.
+integer=[1,3,5,7,9,11,13,15,17,19]
+oddsum=sum(integer)
+print("The sum of first 10 odd integers is :",oddsum)
+
+#The sum of first 10 odd integers by the help of range.
+a=range(1,20,2)
+oddsum1=sum(a)
+print("The sum of first 10 odd integers is :",oddsum1)

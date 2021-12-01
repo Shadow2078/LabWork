8396eb0c4d9d97b6a947a342ef02332fcd2e62a9
Shadow2078 <karkisuman2021@gmail.com>
2021-12-01 20:31:56 +0545

    It's lab 6

---
+++ lab6.py
@@ -0,0 +1,10 @@
+#The sum of first ten even numbers of positive integers using List
+list=[2,4,6,8,10,12,14,16,18,20,22]
+evensum= sum(list)
+print("The sum of first ten even numbers of positive integers is : ",evensum)
+
+
+#Using range
+rg=range(0,23,2)
+evensum1=sum(rg)
+print("The sum of first ten even integers is :",evensum1)

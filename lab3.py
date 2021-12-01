acc3c7260f119caffd798b0dd90f78a8978306a5
Shadow2078 <karkisuman2021@gmail.com>
2021-12-01 13:23:42 +0545

    LabWorks from Humagain Sir

---
+++ lab3.py
@@ -0,0 +1,5 @@
+length=int(input("Enter length of a cuboid"))
+breadth=int(input("Enter breadth of a cuboid"))
+height=int(input("Enter height of a cuboid"))
+Area= length*breadth*height
+print(f"The Area of cuboid is",Area,"cm\u00b3")

f0524b984ce61397e176a496e9cd28af46267431
Shadow2078 <karkisuman2021@gmail.com>
2021-12-03 12:33:06 +0545

    labwork day1

---
+++ Day1/lab1.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a number"))
+y=int(input("Enter other  number"))
+z=int(input("Enter another number"))
+
+sum=x+y+z
+print("The Sum is:", sum)
+print(f"the sum of three numbers is:",sum)
---
+++ Day1/lab3.py
@@ -0,0 +1,6 @@
+#Area of a Cuboid
+length=int(input("Enter length of a cuboid"))
+breadth=int(input("Enter breadth of a cuboid"))
+height=int(input("Enter height of a cuboid"))
+Area= length*breadth*height
+print(f"The Area of cuboid is",Area,"cm\u00b3")
